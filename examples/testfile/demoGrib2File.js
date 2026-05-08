3908db473f0ed2ea2404585da8edae9648667553
Khoa Le <dle5@tulane.edu>
2026-05-08 15:33:32 -0500

    Adding files for sprint 3 and ready to demo

--- examples/testfile/demoGrib2File.js
+++ examples/testfile/demoGrib2File.js
@@ -6,37 +6,21 @@ import { scan, extract, extractOutput } from '../../wasm/webparsers-api.js';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const root = resolve(__dirname, '../..');
 
-function loadWasmFactory() {
-  const code = readFileSync(resolve(root, 'wasm/webparsers.js'), 'utf8');
-  const fakeModule = { exports: {} };
-  // eslint-disable-next-line no-new-func
-  new Function('module', 'exports', code)(fakeModule, fakeModule.exports);
-  const factory = fakeModule.exports?.default ?? fakeModule.exports;
-  if (typeof factory !== 'function') {
-    throw new Error('Could not extract WebParsers factory from webparsers.js');
-  }
-  return factory;
-}
+const file = new Uint8Array(readFileSync(resolve(root, 'examples/timeseries/icon_global_icosahedral_single-level_2026050800_003_T_2M.grib2')));
 
-const rawFactory = loadWasmFactory();
-const wasmBinary = readFileSync(resolve(root, 'wasm/webparsers.wasm'));
-const wf = { wasmFactory: () => rawFactory({ wasmBinary }) };
+console.log('--- SCAN ---');
+const meta = await scan(file);
+console.log(JSON.stringify(meta, null, 2));
 
-const file = new Uint8Array(readFileSync(resolve(root, 'examples/timeseries/gfs_timeseries.grb2')));
-
-// console.log('--- SCAN ---');
-// const meta = await scan(file, wf);
-// console.log(JSON.stringify(meta, null, 2));
-
-// console.log('\n--- EXTRACT (Berlin: 52.52, 13.40) ---');
-// const v = meta.variables.find(x => x.supported).name;
-// const point = await extract(file, { ...wf, variable: v, lat: 52.52, lon: 13.40 });
-// console.log(JSON.stringify(point, null, 2));
+console.log('\n--- EXTRACT (Berlin: 52.52, 13.40) ---');
+const v = meta.variables.find(x => x.supported).name;
+const point = await extract(file, { variable: v, lat: 52.52, lon: 13.40 });
+console.log(JSON.stringify(point, null, 2));
 
 // console.log('\n--- CSV ---');
-// console.log(await extractOutput(file, { ...wf, variable: v, lat: 52.52, lon: 13.40 }, 'csv'));
+// console.log(await extractOutput(file, { variable: v, lat: 52.52, lon: 13.40 }, 'csv'));
 
-const json = await extractOutput(file, { ...wf, variable: ['Pressure', 'Cloud ice'], t1: 0, t2: 1 }, 'json');
-const outputPath = resolve(root, 'examples/testfile/test.json');
-writeFileSync(outputPath, json);
-console.log(`Wrote ${outputPath}`);
+// const json = await extractOutput(file, { variable: ['Pressure', 'Cloud ice'], t1: 0, t2: 1 }, 'json');
+// const outputPath = resolve(root, 'examples/testfile/test.json');
+// writeFileSync(outputPath, json);
+// console.log(`Wrote ${outputPath}`);
--- examples/testfile/demoNetcdf3File.js
+++ examples/testfile/demoNetcdf3File.js
@@ -19,28 +19,12 @@ import { scan, extract } from '../../wasm/webparsers-api.js';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const root      = resolve(__dirname, '../..');
 
-/* ---- Bootstrap the WASM factory the same way demoGrib2File.js does ---- */
-function loadWasmFactory() {
-  const code = readFileSync(resolve(root, 'wasm/webparsers.js'), 'utf8');
-  const fakeModule = { exports: {} };
-  // eslint-disable-next-line no-new-func
-  new Function('module', 'exports', code)(fakeModule, fakeModule.exports);
-  const factory = fakeModule.exports?.default ?? fakeModule.exports;
-  if (typeof factory !== 'function') {
-    throw new Error('Could not extract WebParsers factory from webparsers.js');
-  }
-  return factory;
-}
-
-const rawFactory = loadWasmFactory();
-const wasmBinary = readFileSync(resolve(root, 'wasm/webparsers.wasm'));
-const wf = { wasmFactory: () => rawFactory({ wasmBinary }) };
 
 /* ---- Pick the fixture --------------------------------------------------
  * Drop your NetCDF3 (.nc / .nc3) file into examples/netcdf/ and set the
  * filename here.
  * ---------------------------------------------------------------------- */
-const FIXTURE_PATH = 'examples/netcdf/'; // TODO: set to your <fixture>.nc3
+const FIXTURE_PATH = 'examples/netcdf/nws_precip_last24hours_conus.nc'; // TODO: set to your <fixture>.nc3
 
 /* ---- Reference point (change to match your fixture's coverage) ------- */
 const REF_LAT = 0;   // TODO: pick a lat inside your grid
@@ -49,7 +33,7 @@ const REF_LON = 0;   // TODO: pick a lon inside your grid
 const file = new Uint8Array(readFileSync(resolve(root, FIXTURE_PATH)));
 
 console.log('--- SCAN ---');
-const meta = await scan(file, wf);
+const meta = await scan(file);
 console.log(JSON.stringify(meta, null, 2));
 
 const v = meta.variables.find(x => x.supported);
@@ -58,7 +42,6 @@ if (!v) {
 } else {
   console.log(`\n--- EXTRACT '${v.name}' values [0..20] @ (lat=${REF_LAT}, lon=${REF_LON}) ---`);
   const result = await extract(file, {
-    ...wf,
     variable: v.name,
     lat: REF_LAT, lon: REF_LON,
     t1: 0, t2: 20,
