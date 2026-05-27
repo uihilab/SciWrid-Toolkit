fa1f971671436ac2088582d42cb344af831668d8
Khoa Le <dle5@tulane.edu>
2026-05-26 21:00:13 -0500

    clean up code

--- wasm/build.py
+++ wasm/build.py
@@ -8,13 +8,13 @@ cmd = [
     sys.executable, EMCC,
     "-O2", "-std=c99", "-I..",
     "-sEXPORTED_RUNTIME_METHODS=['ccall','cwrap','UTF8ToString','stringToUTF8','lengthBytesUTF8','HEAPF32','HEAPF64','HEAPU8']",
-    "-sEXPORTED_FUNCTIONS=['_wp_scan','_wp_scan_num_vars','_wp_scan_get_vars_json','_wp_scan_free',"
+    "-sEXPORTED_FUNCTIONS=['_wp_scan','_wp_scan_get_vars_json','_wp_scan_free',"
     "'_wp_scan_messages_layout',"
     "'_wp_normalize','_wp_nc3_scan','_wp_nc3_scan_get_vars_json','_wp_nc3_normalize','_wp_nc3_scan_free',"
     "'_wp_nc3_full_layout',"
     "'_wp_open_from_float_arrays',"
-    "'_wp_close','_wp_variable_name','_wp_nx','_wp_ny','_wp_nt','_wp_is_timeseries',"
-    "'_wp_ds_lats_ptr','_wp_ds_lons_ptr','_wp_ds_times_ptr','_wp_ds_data_ptr',"
+    "'_wp_close','_wp_nx','_wp_ny','_wp_nt',"
+    "'_wp_ds_lats_ptr','_wp_ds_lons_ptr','_wp_ds_data_ptr',"
     "'_wp_find_nearest_lat','_wp_find_nearest_lon','_wp_query','_wp_free','_wp_malloc','_wp_memcpy',"
     "'_malloc','_free']",
     "-sALLOW_MEMORY_GROWTH=1",
